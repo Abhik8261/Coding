25d9efdd437c8ca0abd6a504ba1e9622d5aa4797
Abhik8261 <abhik.tech8261@gmail.com>
2025-05-23 22:32:01 +0100

    count & say

---
+++ Leetcode/String/Count and Say.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+string say(string s){
+    if(s.size()==0)
+    return "1";
+    string ans="";
+    int len=s.size();
+    for(int i=0;i<len;i++){
+        int cnt=1;
+        while(i<len-1 && s[i]==s[i+1]){
+            i++;
+            cnt++;
+        }
+        ans+=to_string(cnt);
+        ans+=s[i];
+    }
+
+    return ans;
+
+}
+    string countAndSay(int n) {
+        string ans="";
+        while(n--){
+            ans=say(ans);
+        }
+        return ans;
+    }
+};
